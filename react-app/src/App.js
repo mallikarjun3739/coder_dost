10f5099c7c2c885055d34c3963eb0fb3c03946fd
mallikarjun3739 <mallikarjun16455@gmail.com>
2023-04-26 22:58:11 +0530

    codeee

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,9 +2,17 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="App-header">Hello World</div>
+      <div className="App-header">Hello World
+      <Demo/>
+      </div>      
     </div>
   );
 }
-
+function Demo() {
+  return (
+    <div className="App">
+      <div className="App-header">Demo Component</div>
+    </div>
+  );
+}
 export default App;
